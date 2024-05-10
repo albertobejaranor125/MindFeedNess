528a1a81d6bb6c135a01e8685ebf1aac4a8a6be9
albertobejaranor125 <albertobejaranor@gmail.com>
2024-05-10 15:49:36 +0200

    Partial Implemented

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -1,19 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class PlayManager : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject checkPoints;
+    
+    private Dictionary<Transform, List<List<Transform>>> allCheckpointsMap = new Dictionary<Transform, List<List<Transform>>>();
+    
+    /*struct Value
+    {
+        List<GameObject> path1;
+        List<GameObject> path2;
+        List<GameObject> path3;
+    }
+    private Value[] allValues;*/
     // Start is called before the first frame update
     void Start()
     {
         
+
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            moveToNextPoint();
+        }
+    }
+
+    private void moveToNextPoint()
+    {
+        player.transform.position = Vector3.Lerp(allCheckpointsMap.Keys,,(float) DateTime.Now.Minute);
     }
 }
