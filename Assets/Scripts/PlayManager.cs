f9bf291bf23f702b024d997550affaea75927b2a
albertobejaranor125 <albertobejaranor@gmail.com>
2024-05-22 12:24:21 +0200

    Fix Bug Attempt

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using static UnityEngine.Rendering.DebugUI;
@@ -11,10 +12,39 @@ public class PlayManager : MonoBehaviour
 {
     [SerializeField] private GameObject player;
     [SerializeField] private Transform checkPoints;
-    private Dictionary<Transform, List<List<Transform>>> allCheckpointsMap = new Dictionary<Transform, List<List<Transform>>>();
+    struct CheckPoint
+    {
+        Transform node;
+        float time;
+        public Transform Nodo
+        {
+            get
+            {
+                return node;
+            }
+            set
+            {
+                node = value;
+            }
+        }
+        public float Tiempo
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                time = value;
+            }
+        }
+    }
+    private Dictionary<Transform, List<List<CheckPoint>>> allCheckpointsMap = new Dictionary<Transform, List<List<CheckPoint>>>();
     Transform DecisionActual;
     int PathActual;
     int NodoActual;
+    static float velocidad = 5f;
+    //List<CheckPoint> checkPointList = new List<CheckPoint>();
     // Start is called before the first frame update
     void Start()
     {
@@ -30,23 +60,40 @@ public class PlayManager : MonoBehaviour
     void throughDecisionNode(Transform decisionNode) //decision Node = Nodo negro
     {
         //añadir decision node como clave de mi estructura
-        allCheckpointsMap.Add(decisionNode, new List<List<Transform>>());
-        
+        allCheckpointsMap.Add(decisionNode, new List<List<CheckPoint>>());
+        //List<CheckPoint> checkPointList = new List<CheckPoint>();
         //para cada Nodo hijo tipo "Path"        
         for (int i = 0; i < decisionNode.childCount; ++i)
         {
             
             List<Transform> path = throughPathNode(decisionNode.GetChild(i));
+            List<CheckPoint> checkPointList = new List<CheckPoint>();
             if (path.Count > 0)
             {
+                //distancia en metros
+                /*float distancia = (path[i].position - decisionNode.position).magnitude;
+                float tiempo = distancia / velocidad;
+                CheckPoint checkPointNew = new CheckPoint();
+                checkPointNew.Nodo = path[i];
+                checkPointNew.Tiempo = tiempo;*/
+                for(int j = 0; j < path.Count; j++)
+                {
+                    float distancia = (path[j].position - (j == 0 ? decisionNode.position : path[j - 1].position)).magnitude;
+                    float tiempo = distancia / velocidad;
+                    CheckPoint checkPointNew = new CheckPoint();
+                    checkPointNew.Nodo = path[j];
+                    checkPointNew.Tiempo = tiempo;
+                    checkPointList.Add(checkPointNew);
+                }
                 
-                allCheckpointsMap[decisionNode].Add(path); //añadir un camino
+                //checkPointList.Add(checkPointNew);
+                allCheckpointsMap[decisionNode].Add(checkPointList); //añadir un camino
                                                            //recursividad para los hijos de hijos
                 throughDecisionNode(path[path.Count - 1]);
             }
             else
             {
-                allCheckpointsMap[decisionNode].Add(path);
+                allCheckpointsMap[decisionNode].Add(checkPointList); //añadir un camino
             }
         }
     }
@@ -63,9 +110,8 @@ public class PlayManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    static DateTime _iniciorecorrido;
-    static float _tiempoPaseo = 10000;
-    static Transform initi, end;
+    static DateTime _lastCheckpoint;
+    CheckPoint initi, end;
     void Update()
     {
         //deberias conocer (variable global) el nodo de decision actual y nodo transitorio actual
@@ -73,14 +119,14 @@ public class PlayManager : MonoBehaviour
         {
             PathActual = 0;
             NodoActual = -1;//-1;
-            _iniciorecorrido = DateTime.Now;
+            _lastCheckpoint = DateTime.Now;
         }
 
         if (PathActual == -1 && Input.GetKey(KeyCode.RightArrow))
         {
             PathActual = 1;
             NodoActual = -1;//-1;
-            _iniciorecorrido = DateTime.Now;
+            _lastCheckpoint = DateTime.Now;
         }
 
         if (PathActual != -1)
@@ -91,37 +137,35 @@ public class PlayManager : MonoBehaviour
 
     private void moveToNextPoint()
     {
-        List<Transform> path = allCheckpointsMap[DecisionActual][PathActual];
-        float partialTime = _tiempoPaseo / path.Count;
-
-        //Transform initi, end;
+        List<CheckPoint> path = allCheckpointsMap[DecisionActual][PathActual];
         if (NodoActual == -1) 
         {
-            initi = DecisionActual;
+            initi.Nodo = DecisionActual;
+            initi.Tiempo = 0;
             end = path[0];
         }
-        else if(NodoActual <= path.Count)
+        else if(NodoActual < path.Count)
         {
             initi = path[NodoActual];
             end = path[NodoActual+1];
         }
-        
-         
-        float ratio = (float)(DateTime.Now - _iniciorecorrido).TotalMilliseconds / partialTime;
+ 
+        float ratio = (float)(DateTime.Now - _lastCheckpoint).TotalMilliseconds / (end.Tiempo*1000);
         Debug.Log(ratio);
-        player.transform.position = Vector3.Lerp(initi.position, end.position, ratio);
-        Vector3 initRotation = new Vector3(initi.rotation.x, initi.rotation.y, initi.rotation.z);
-        Vector3 endRotation = new Vector3(end.rotation.x, end.rotation.y, end.rotation.z);
-        //player.transform.rotation = Quaternion.FromToRotation(initRotation, endRotation);
-        player.transform.rotation = Quaternion.Lerp(initi.rotation, end.rotation, ratio);
+        player.transform.position = Vector3.Lerp(initi.Nodo.position, end.Nodo.position, ratio);
+        Vector3 haciaDondeMiroAhora = player.transform.forward;
+        Vector3 haciaDondeQuieroMirar = (end.Nodo.position - initi.Nodo.position).normalized;
+        Quaternion final = Quaternion.FromToRotation(haciaDondeMiroAhora, haciaDondeQuieroMirar);
+        player.transform.rotation = final;
         //si ratio = 1 -> hay cambair caminoActual por CaminaoActual+
         if (ratio >= 1)
         {
             NodoActual++;
+            _lastCheckpoint = DateTime.Now;
         }
         if (NodoActual == path.Count-1)
         {
-            DecisionActual = path[NodoActual];
+            DecisionActual = path[NodoActual].Nodo;
             PathActual = -1;
             NodoActual = -1;
         }
