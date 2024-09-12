2eacf546f3c00ff61d749cb48023f114cc1ebfc2
albertobejaranor125 <albertobejaranor@gmail.com>
2024-09-12 19:51:32 +0200

    FIX

--- Assets/Scripts/PlayManager.cs
+++ Assets/Scripts/PlayManager.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using static UnityEngine.Rendering.DebugUI;
